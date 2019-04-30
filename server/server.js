34611f845a59f76c882d69e8decd333209639bcc
Amie Thao <amie.c.thao@gmail.com>
2019-04-30 08:33:59 -0500

    testing weather app

--- server/server.js
+++ server/server.js
@@ -1,22 +1,16 @@
-
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 
-require('dotenv').config();
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
-app.use(express.static('build'));
-
-app.get('/', (req, res) => {
-  // API call
-
-})
+app.use(express.static("build"));
 
 
 // Listens for server to start
-app.listen(PORT,  () => {
-  console.log('Listening on port: ', PORT);
+app.listen(PORT, () => {
+  console.log("Listening on port: ", PORT);
 });
--- src/index.js
+++ src/index.js
@@ -4,7 +4,29 @@ import './index.css';
 import App from './components/App/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+// Provider allows us to use redux within our react app
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+
+// Reducer that holds our results
+const random = (state = {}, action) => {
+    if(action.type === 'SET_RANDOM') {
+        return action.payload;
+    }
+    return state;
+}
+
+// Create one store that all components can use
+const storeInstance = createStore(
+    combineReducers({
+        random,
+    }),
+    applyMiddleware(logger),
+);
+
+ReactDOM.render(<Provider store={storeInstance}><App /></Provider>, 
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
